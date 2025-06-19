d1a9a2987aa7ec874f7bfc38ff94cd37f6bc38fd
serhii-123 <plotva10@gmail.com>
2025-06-19 15:14:09 +0300

    Some commit

--- 04_Telegram_Echo/index.ts
+++ 04_Telegram_Echo/index.ts
@@ -3,9 +3,20 @@ import { Bot } from 'grammy';
 const token = '';
 const bot = new Bot(token);
 
+bot.hears('photo', c => {
+    const url = `https://picsum.photos/200/300?random=${Date.now()}`;
+
+    console.log(
+        'Користувач ' + c.chat.first_name
+        + (c.chat.last_name ? c.chat.last_name : '')
+        + ' запросив зображення'
+    );
+    c.replyWithPhoto(url);
+});
+
 bot.on('message:text', c => {
     console.log(
-        'Користувач' + c.chat.first_name
+        'Користувач ' + c.chat.first_name
         + (c.chat.last_name ? c.chat.last_name : '')
         +' написав ' + c.message.text
     );
