4904cafef6ec7bfec8b5acd5fd36b1f9eb18391e
serhii-123 <plotva10@gmail.com>
2025-06-30 12:45:57 +0300

    Some commit

--- 07_Google_Uploader/index.ts
+++ 07_Google_Uploader/index.ts
@@ -1,56 +1,47 @@
-import { confirm, input } from "@inquirer/prompts";
 import { config } from "dotenv";
-import authorize from "./authorize";
-import uploadFile from "./uploadFile";
-import shortenLink from "./shortenLink";
 import FileUtils from "./FileUtils";
+import GoogleUtils from "./GoogleUtils";
+import TinyUrlUtils from "./TinyUrlUtils";
 
 config();
 
+const tinyUrlToken: string = process.env.TINYURL_TOKEN as string;
 const folderId: string = process.env.FOLDER_ID as string;
 const redirectURI: string = process.env.REDIRECT_URI as string;
 const refreshToken: string = process.env.REFRESH_TOKEN as string;
 const clientId: string = process.env.CLIENT_ID as string;
 const clientSecret: string = process.env.CLIENT_SECRET as string;
-const clientEmail: string = process.env.CLIENT_EMAIL as string;
 
 async function start(
     folderId: string,
     redirectURI: string,
     refreshToken: string,
     clientId: string,
-    clientSecret: string,
-    clientEmail: string
+    clientSecret: string
 ): Promise<void> {
-    const auth = await authorize(redirectURI, refreshToken, clientId, clientEmail, clientSecret);
-    const filePath: string = await FileUtils.getFilePath();
-    let fileName: string = await FileUtils.getFileNameFromPath(filePath);
-    const fileExtension = filePath.slice(filePath.lastIndexOf('.') + 1);
+    const auth = await GoogleUtils.authorize(redirectURI, refreshToken, clientId, clientSecret);
+    const filePath: string = await FileUtils.getPath();
+    let fileName: string = await FileUtils.getNameByPath(filePath);
+    const fileExtension: string = await FileUtils.getExtensionByFileStr(fileName);
     
-    console.log(
-        `Path to file: ${filePath}\n` 
-        + `File name: ${fileName}\n`
-        + `File extension: ${fileExtension}`
-    );
+    await FileUtils.displayInfo(filePath, fileName, fileExtension);
 
-    const rename: boolean = await confirm({
-        message: `You're uploading the file with the name: ${fileName}. Would you like to change it?`
-    });
+    const rename: boolean = await FileUtils.askForRename(fileName);
 
     if(rename)
-        fileName = await input({
-            message: 'Enter new file name (WITHOUT extension aka .jpg, .png etc.)'
-        });
+        fileName = await FileUtils.getNewName();
 
-    const url = await uploadFile(auth, filePath, fileName, folderId);
+    const fileId = await GoogleUtils.uploadFile(auth, filePath, fileName, folderId);
     console.log('Successfully uploaded!');
     
-    const shortify: boolean = await confirm({ message: 'Would you like to shorten you link?' });
+    const shortify: boolean = await TinyUrlUtils.askForShortneLink();
 
     if(!shortify) return;
+    
+    const fileUrl: string = `https://drive.google.com/file/d/${fileId}/view`; 
+    const shortFileUrl: string = await TinyUrlUtils.shortenLink(tinyUrlToken, fileUrl);
 
-    const shortUrl: string = await shortenLink(url);
-    console.log('Your short link is: ' + shortUrl);
+    console.log('Your short link is: ' + shortFileUrl);
 }
 
-start(folderId, redirectURI, refreshToken, clientId, clientSecret, clientEmail);
+start(folderId, redirectURI, refreshToken, clientId, clientSecret);
